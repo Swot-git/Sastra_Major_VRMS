019eab7597348be5500712317e3fdb9a1926d696
Swot-git <swathigit23@gmail.com>
2024-07-09 23:11:27 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDLcbK061cGjKqF3KNN35aenSXs8OyTp5k",
-  authDomain: "vrms-rentalease.firebaseapp.com",
-  projectId: "vrms-rentalease",
-  storageBucket: "vrms-rentalease.appspot.com",
-  messagingSenderId: "802632171958",
-  appId: "1:802632171958:web:4129b9cbdc0995ee024117"
+  //paste firebase config
 };
 
 
